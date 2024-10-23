523f4a41453060e89d9786755fa20b2b16a2e0c3
divineProjects <divinejigu@gmail.com>
2024-10-24 00:28:56 +0200

    modified added loadHeaderFooter() to main.js

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -32,8 +32,38 @@ export function getParam(param){
 
 export function renderListWithTemplate(templateFn, parentElement, list, position= "afterbegin", clear = false){
   const htmlStrings = list.map(templateFn);
+  
   if (clear){
     parentElement.innerHTML = "";
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+function renderWithTemplate(template, parent, data={}, callback){
+  parent.insertAdjacentHTML("afterbegin", template);
+  if(callback){
+    callback(data);
+  }
+}
+
+// function to take an optional object and a template and insert the objects as HTML into the DOM
+export async function loadTemplate(path){
+  const html = await fetch(path);
+  const htmlText = await html.text();
+  return htmlText;
+}
+
+// function to dynamically load the header and footer into a page
+export async function loadHeaderFooter() {
+  // Load the header and footer templates in from our partials (loadTemplate).
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+
+  // Grab the header and footer elements out of the DOM.
+  const headerElement = document.querySelector("#main-header");
+  const footerElement = document.querySelector("#main-footer");
+
+  // Render the header and footer (renderWithTemplate).
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
 }
